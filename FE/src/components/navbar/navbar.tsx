79913594d14c9e16e30500e97169f67514f1de4b
Jacopo-Giola <jacky.giola@gmail.com>
2025-09-01 17:44:28 +0200

    navbar_hamburger

--- FE/src/components/navbar/navbar.tsx
+++ FE/src/components/navbar/navbar.tsx
@@ -1,15 +1,27 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import logo3 from '../../assets/logo3.jpg'
 import './style.css'
 
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => setOpen(!open);
+  const closeMenu = () => setOpen(false);
+
   return (
     <nav className='navbar'>
-      <Link to="/" ><img src={logo3} alt="" /></Link>
-      <div className="items">
-        <Link to="/" >MASSOTERAPIA</Link>
-        <Link to="/contatti">CONTATTI</Link>
-        <Link to="/newsletter">NEWSLETTER</Link>
+      <Link to="/" onClick={closeMenu}><img src={logo3} alt="" /></Link>
+      <div className={`hamburger ${open ? "open" : ""}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+       <div className={`items ${open ? "active" : ""}`}>
+        <Link to="/" onClick={closeMenu}>MASSOTERAPIA</Link>
+        <Link to="/contatti" onClick={closeMenu}>CONTATTI</Link>
+        <Link to="/newsletter" onClick={closeMenu}>NEWSLETTER</Link>
       </div>
     </nav>
   );
